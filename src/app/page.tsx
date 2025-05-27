c3cb89119055b0a2b316d56977fa82e4e2513bb6
noahsunpro <noah.vass@news.com.au>
2025-05-27 13:44:53 +1000

    feature: tag on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,16 +29,25 @@ const HomePage = async () => {
           className="w-full h-64 object-cover rounded-lg mb-8"
         />
       )}
-      <h1 className="text-4xl font-bold text-center mb-8">Nar Writes</h1>
+      <h1 className="text-4xl font-bold text-center mb-2">Nar Writes</h1>
       <div className="space-y-4">
         {posts.map((post) => (
           <div key={post.node._sys.filename} className="p-6 border-b">
-            <h2 className="text-2xl font-semibold">
-              <Link href={`/blog/${post.node._sys.filename}`}>
-                <p className="text-blue-600 hover:underline">{post.node.title}</p>
-              </Link>
-            </h2>
-            <p>{post.node.body.children[0].children[0].text}...</p>
+            <div className="flex items-center justify-between flex-wrap gap-2">
+              <h2 className="text-2xl font-semibold">
+                <Link href={`/blog/${post.node._sys.filename}`}>
+                  <p className="text-gray-900 hover:underline">{post.node.title}</p>
+                </Link>
+              </h2>
+              <div className="flex flex-wrap gap-2 text-sm text-gray-600">
+                {post.node.tags?.sort().map((tag) => (
+                  <span key={tag} className="px-2 py-1 rounded-full">
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <p>{post.node.body.children[0].children[0].text}..</p>
           </div>
         ))}
       </div>
